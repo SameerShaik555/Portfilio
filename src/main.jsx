bf537d4af58125d6bf2a8c99ba246973255860bd
unknown <sameershaiksam98@gmail.com>
2024-01-19 17:26:16 +1100

    Your commit message

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { jsxDEV } from "react/jsx-dev-runtime";
+
+
 
 import App from "./App";
 import "./index.css";
--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vue()],
-  build: {
-    target: 'esnext',
-    outDir: 'dist',
-    assetsDir: 'assets',
-    minify: 'terser',
-    sourcemap: true,
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        // Update this line to resolve the issue
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom'],
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
-});
+  plugins: [react()],
+})
